efde45db1c28f4797451b012a720bae3b801cb6b
mccreery-john-student <70606724+mccreery-john-student@users.noreply.github.com>
2024-06-08 18:28:30 -0400

    Realized 1/4x label was not anchored properly, fixed now

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -390,6 +390,7 @@
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(965, 260);
